43a8d9425ade22f496c5af31610e58c7ca2e2021
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2021-01-24 17:12:19 +0530

    frame

---
+++ main.py
@@ -0,0 +1,77 @@
+import PySimpleGUI as sg
+import pprint
+import os.path
+
+
+def show_diagnostic_results():
+    return
+
+
+def format_output(json):
+    return
+
+
+def load_window():
+    # First the window layout in 2 columns
+    input_column = [
+        [
+            sg.Text("Ransomware File     "),
+            sg.In(size=(65, 1), enable_events=True, key="-FILE-"),
+            sg.FileBrowse(),
+        ],
+        [
+            sg.Text("Ransomware Name     "),
+            sg.In(size=(65, 1), enable_events=True, key="-NAME-"),
+
+        ],
+        [
+            sg.Text("Ransomware Extension"),
+            sg.In(size=(65, 1), enable_events=True, key="-EXTENSION-"),
+        ],
+        [
+            sg.Button('Submit')
+        ],
+        [
+            sg.HSeparator()
+        ],
+        [
+            sg.Text("Input instructions")
+        ]
+
+    ]
+
+    results_viewer_column = [
+        [sg.Text("Diagnostics Results")],
+        [sg.Text(size=(100, 1), key="-TOUT-")],
+        [sg.Image(key="-IMAGE-")],
+    ]
+
+    # ----- Full layout -----
+    layout = [
+        [
+            sg.Column(input_column),
+            sg.VSeperator(),
+            sg.Column(results_viewer_column),
+        ]
+    ]
+
+    window = sg.Window("Ransomware Solution Recomendation System", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
+        if event == "Submit":
+            file = values["-FILE-"]
+            name = values["-NAME-"]
+            extension = values["-EXTENSION-"]
+
+            # function call to main func
+            '''
+            print(file)
+            print(name)
+            print(extension)
+            '''
+
+            window["-TOUT-"].update(format_output())
